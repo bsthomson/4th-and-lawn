6d87b3ced07cf7ac3a050dd68742fcf8775b1a9f
unknown <thomson.bs@gmail.com>
2018-10-04 19:36:02 -0500

    Controllers for models

---
+++ controllers/rentersController.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+module.exports = {
+  findall: (req, res) => {
+    db.Renter
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: (req, res) => {
+    db.Renter
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: (req, res) => {
+    db.Renter
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: (req, res) => {
+    db.Renter
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: (req, res) => {
+    db.Renter
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
