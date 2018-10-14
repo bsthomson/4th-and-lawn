5bba2ebe321984eafe0b2ec57696f56e643b3e40
unknown <thomson.bs@gmail.com>
2018-10-14 16:05:00 -0500

    removed unnecessary files

--- models/index.js
+++ models/index.js
@@ -2,6 +2,5 @@ module.exports = {
   User: require("./User"),
   ParkingSpot: require("./ParkingSpot"),
   Renter: require("./Renter"),
-  Jayhawk: require("./Jayhawk"),
-  // JayhawkSeed: require("./JayhawkSeed"),
+  Jayhawk: require("./Jayhawk")
 }
