fcf839d523dafdc9a123bb75dc8a00a8a76c787c
unknown <thomson.bs@gmail.com>
2018-10-16 19:07:42 -0500

    added a route

--- client/src/components/form-post-parking/post-parking.js
+++ client/src/components/form-post-parking/post-parking.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import moment from "moment";
-// import eventcall from "../../utils/eventcall";
 
 class PostParkingSpot extends Component {
     constructor() {
--- routes/renterRoutes.js
+++ routes/renterRoutes.js
@@ -7,30 +7,40 @@ const ParkingSpot = db.ParkingSpot
 module.exports = function (app) {
 
   // route for posting your rental information for renting a parking spot
-  app.post('/api/rentthisspot/:id', (req, res) => {
-    console.log(req.body)
+  app.route('/api/rentthisspot/:id')
+    .post( (req, res) => {
+      console.log(req.body)
 
-    const { licenseplate, make, model, date, time } = req.body;
+      const { licenseplate, make, model, date, time } = req.body;
 
-    Renter.create({
-      licenseplate: licenseplate,
-      make: make,
-      model: model,
-      date: date,
-      time: time,
-      user: req.session.passport.user,
-      parkingspot: req.params.id
+      Renter.create({
+        licenseplate: licenseplate,
+        make: make,
+        model: model,
+        date: date,
+        time: time,
+        user: req.session.passport.user,
+        parkingspot: req.params.id
+      })
+        .then( dbRenter => {
+          console.log("User: ", dbRenter)
+          User.findOneAndUpdate({ _id: req.session.passport.user }, { $push: { rentedspots: req.params.id, rentinfo: dbRenter._id } }).exec();
+          ParkingSpot.findOneAndUpdate({ _id: req.params.id }, { $push: { renter: req.session.passport.user, rentinfo: dbRenter._id } }).exec();
+          res.send(dbRenter)
+        })
+        .catch( err => {
+          res.json(err)
+        })
     })
-      .then( dbRenter => {
-        console.log("User: ", dbRenter)
-        User.findOneAndUpdate({ _id: req.session.passport.user }, { $push: { rentedspots: req.params.id, rentinfo: dbRenter._id } }).exec();
-        ParkingSpot.findOneAndUpdate({ _id: req.params.id }, { $push: { renter: req.session.passport.user, rentinfo: dbRenter._id } }).exec();
-        res.send(dbRenter)
-      })
-      .catch( err => {
-        res.json(err)
-      })
-  })
+    .get( (req, res) => {
+      ParkingSpot.findOne({ _id: req.params.id})
+        .then( dbParkingSpot => {
+          res.json(dbParkingSpot)
+        })
+        .catch( err => {
+          res.json(err)
+        })
+    })
 
   // route that gets all of the users rented spots
   app.get('/api/rentedspots', (req, res) => {
