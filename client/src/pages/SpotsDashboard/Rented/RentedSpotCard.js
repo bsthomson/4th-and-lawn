14111b4c6b2b9ebdb0e7f8c2a426a44bd52d1193
Terran Blake <terranblake@gmail.com>
2018-11-26 21:00:23 -0600

    Added directions link button

--- client/src/pages/SpotsDashboard/Rented/RentedSpotCard.js
+++ client/src/pages/SpotsDashboard/Rented/RentedSpotCard.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import moment from "moment";
+
+const encodeMapURL = (parkingLocation, userLocation) => {
+    console.log({ parkingLocation });
+
+    return (
+        "https://www.google.com/maps/dir/" +
+        userLocation.split(' ').join('+') + "/" +
+        parkingLocation.split(' ').join('+')
+    )
+}
 
 const RentedSpotCard = props => {
     return (
-        <section key = { props._id } >
+        <section key={props._id} >
             <div className="dashboard__user-item">
                 <div className="dashboard__user-address">
                     <div className="row" style={{ color: "white" }}>
@@ -19,13 +30,19 @@ const RentedSpotCard = props => {
                         </div>
                     </div>
                 </div>
+
                 <div className="dashboard__user-buttons">
+                    <a href={encodeMapURL("Current Location", props.address)}>
+                        <div className="dashboard-card__button dashboard-card__button--view" >
+                            <span className="spot--test"><i class="fas fa-map-marker-alt spot--icon"></i></span>
+                        </div>
+                    </a>
                     <div className="dashboard-card__button dashboard-card__button--delete" onClick={() => props.deleteRented(props._id)}>
                         <span className="spot--test"><i className="fas fa-trash-alt spot--icon"></i></span>
                     </div>
                 </div>
             </div>
-    </section >
+        </section >
     )
 }
 
